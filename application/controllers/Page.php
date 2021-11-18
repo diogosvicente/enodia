e994886f488f3305fecb2c280fe531aafcffd4c8
Diogo <diogosk84ever@hotmail.com>
2021-11-17 23:41:15 -0300

    teste

--- application/controllers/Page.php
+++ application/controllers/Page.php
@@ -24,12 +24,20 @@ class Page extends CI_Controller {
 		$crud->set_table("produto");
 		$crud->set_field_upload('imagem','assets/uploads');
 		$crud->required_fields("nome", "preco", "imagem", "descricao");
-		$crud->callback_field('preco',array($this, '_mascara_preco'));
+		$crud->callback_column('preco',array($this,'_callback_coluna_preco'));
+		//$crud->callback_field('preco',array($this,'_callback_preco'));
 		$form = $crud->render();
 		$this->load->view('view_cadastro_produtos',$form);
 	}
 
-	function _mascara_preco($value = '', $primary_key = null){
-    	return '<input name="valor1" type="text" class="mascara" >';
+	function _callback_coluna_preco($value, $row){
+		return "R$ $value";
 	}
+
+	/*function _callback_preco($value = '', $primary_key = null){
+		return '+30 <input type="text" maxlength="50" value="'.$value.'" name="preco" style="width:462px">';
+	}*/
+	
 }
+
+
--- application/views/view_cadastro_produtos.php
+++ application/views/view_cadastro_produtos.php
@@ -8,8 +8,7 @@ $file_new_path = str_replace("index.php/", "",$file);
     <link type="text/css" rel="stylesheet" href="<?php echo $file_new_path; ?>" />
 <?php endforeach; ?>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-maskmoney/3.0.2/jquery.maskMoney.min.js"></script>
+<script src="<?php echo base_url('assets/jquery/jquery.maskMoney.js'); ?>" type="text/javascript"></script>
 
 <div style='height:20px;'></div>  
 <div style="padding: 10px">
@@ -21,13 +20,4 @@ $file_new_path = str_replace("index.php/", "",$file);
     <script src="<?php echo $file_new_path; ?>"></script>
 <?php endforeach; ?>
 
-<script type="text/javascript">
-    $(function(){
-       $(".mascara").maskMoney({
-          prefix: 'R$ ',
-          allowNegative: true,
-          thousands: '.',
-          decimal: ','
-       });
-    }); 
-</script>
+<script type="text/javascript">$("#field-preco").maskMoney({prefix:'R$ ', allowNegative: false, thousands:'.', decimal:'.', affixesStay: false});</script>
